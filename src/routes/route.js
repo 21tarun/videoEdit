f61b0bbd9a556fb6d01cb72ef27bb3602ea78883
21tarun <tarunuday2000@gmail.com>
2023-06-14 14:55:25 +0530

    commit merger videos

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,8 +2,11 @@ const express =require('express')
 const router= express.Router()
 const multer = require('multer');
 const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path
+const ffprobePath = require('@ffprobe-installer/ffprobe').path;
+
 const ffmpeg = require('fluent-ffmpeg')
 ffmpeg.setFfmpegPath(ffmpegPath)
+ffmpeg.setFfprobePath(ffprobePath);
 
 
 
@@ -61,6 +64,35 @@ router.post('/api/videos/:id/trim', (req, res) => {
         })
         .run()
 
+});
+
+router.post('/api/videos/:id/merge', (req, res) => {
+    const videoId = req.params.id;
+    const clipIds = req.body.clipIds; // Array of clip IDs to merge
+  
+    // Path to the input video files
+    const inputPaths = clipIds.map((clipId) => `uploads/${clipId}.mp4`);
+  
+    // Path to the output merged video file
+    const outputPath = `merged/${videoId}-merged.mp4`;
+  
+    const ffmpegCommand = ffmpeg();
+    
+    // Add inputs
+    inputPaths.forEach((inputPath) => {
+      ffmpegCommand.input(inputPath);
+    });
+  
+    // Merge video clips
+    ffmpegCommand.mergeToFile(outputPath)
+      .on('error', (err) => {
+        console.error('Error while merging videos:', err);
+        return res.status(400).send({ status: false, message: 'Error during merging' });
+      })
+      .on('end', () => {
+        console.log('Video merging completed');
+        return res.status(200).send({ status: true, message: 'Merge successful' });
+      });
 });
 
 module.exports=router
